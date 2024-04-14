7c70cf8d80003699aa43cac8edb7df660f126e5a
SamTCrow <162122571+SamTCrow@users.noreply.github.com>
2024-04-14 18:51:17 +0200

    Let's try again

--- src/routes/api/card/+server.ts
+++ src/routes/api/card/+server.ts
@@ -6,21 +6,23 @@ export const prerender = false;
 
 export async function GET({ url, platform }) {
 	const query = url.searchParams.get('q');
+	if (query) {
+		try {
+			if (!platform) {
+				error(502, 'No platform!!');
+			}
 
-	try {
-		if (!platform) {
-			error(404, 'No platform!!');
+			const value = platform.env.MAGIC_CARD.get(query);
+
+			if (value === null) {
+				const card = await getCards(query);
+				platform.env.MAGIC_CARD.put(query, card);
+				return json(card);
+			} else {
+				return json(value);
+			}
+		} catch (e) {
+			error(404, 'Something went wrong.');
 		}
-
-		const value = platform.env.MAGIC_CARD.get(query);
-
-		if (value === null && query !== null) {
-			const card = await getCards(query);
-			platform.env.MAGIC_CARD.put(query, card);
-			return json(card);
-		}
-		return json(value);
-	} catch (e) {
-		error(404, 'Something went wrong.');
 	}
 }
