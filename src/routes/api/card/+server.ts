7bae14f7bbdf57ad61adf5999f8b42024149f78f
SamTCrow <162122571+SamTCrow@users.noreply.github.com>
2024-04-14 18:42:41 +0200

    trying to resolver workers problems

--- src/routes/api/card/+server.ts
+++ src/routes/api/card/+server.ts
@@ -8,16 +8,18 @@ export async function GET({ url, platform }) {
 	const query = url.searchParams.get('q');
 
 	try {
-		if (platform) {
-			const value = platform.env.MAGIC_CARD.get(query);
-
-			if (value === null && query !== null) {
-				const card = await getCards(query);
-				platform.env.MAGIC_CARD.put(query, card);
-				return json(card);
-			}
-			return json(value);
+		if (!platform) {
+			error(404, 'No platform!!');
 		}
+
+		const value = platform.env.MAGIC_CARD.get(query);
+
+		if (value === null && query !== null) {
+			const card = await getCards(query);
+			platform.env.MAGIC_CARD.put(query, card);
+			return json(card);
+		}
+		return json(value);
 	} catch (e) {
 		error(404, 'Something went wrong.');
 	}
